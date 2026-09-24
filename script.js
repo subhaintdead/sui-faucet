8700c893c70af1b765a8b9af5d7bede6f4f47ee0
subhaintdead <subhgotbeans@gmail.com>
2026-09-25 03:47:42 +0530

    its been a long time lol i havent committed yet(still doint the frontend, i have in head what the backend would look like, usually i write te backend before, the exception was one of my first personal sites lol)

---
+++ script.js
@@ -0,0 +1,51 @@
+const form = document.getElementById('faucet-form');
+const button = document.getElementById('sbutton');
+const status = document.getElementById('status');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const address = document.getElementById('address').value.trim();
+
+    const token = document.querySelector('[name=cf-turnstile-response]')?.value;
+
+    if (!address.startsWith('0x') || address.length < 64) {
+        showStatus('gng that aint a valid sui address', 'error');
+        return;
+    } if (!token) {
+        showStatus('Please complete the captcha to continue.', 'error');
+        return;
+    }
+
+    button.disabled = true;
+    showStatus('', '');
+
+    try {
+        const potatoes = await fetch('/api/skibidi', {
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ address, token })
+        });
+
+        const data = await potatoes.json();
+
+        if (potatoes.ok) {
+            button.textContent = 'sent!';
+            showStatus(`SUI sent! Tx: ${data.txHash}`, 'success');
+        } else {
+            button.disabled = false;
+            button.textContent = 'Give me gas!';
+            showStatus(data.error || 'something went wrong.', 'error');
+        }
+         catch (err) {
+            button.disabled = false;
+            button.textcontent = 'Give me gas!';
+            showStatus('network')
+        }
+
+    }
+
+
+}
+
+
+)
